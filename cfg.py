eeba2e9571b1211e8e90fb7b9807ca2cafc8dcd4
xiangzhe_lu <34537848+lxztju@users.noreply.github.com>
2019-09-08 21:08:51 +0800

    update cfg.py

--- cfg.py
+++ cfg.py
@@ -18,6 +18,9 @@ INPUT_SIZE = 224
 #下载地址：https://download.pytorch.org/models/densenet169-b2777c0a.pth
 PRETRAINED_MODEL = './dense169.pth'
 
+##训练完成，权重文件的保存路径,默认保存在trained_model下
+TRAINED_MODEL = './trained_model/dense169_110.pth'
+
 #数据集的存放位置
 TRAIN_DATASET_DIR = './datasets/train_data_v2'
 VAL_DATASET_DIR = './datasets/val_data_v2'
